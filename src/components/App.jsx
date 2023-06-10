8fd7f921afacbf56ad1379b4a830305767ff4e21
Kamila Adamska <121kama121@gmail.com>
2023-06-10 18:38:05 +0200

    Added PropTypes and fixed main container

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import { Profile } from './profile/Profile';
 import user from './profile/user.json';
+import css from './app.module.css';
 
 export const App = () => {
   return (
-    <div class="container">
+    <div className={css.container}>
       <Profile
         username={user.username}
         tag={user.tag}
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,6 +1,13 @@
 import css from './profile.module.css';
+import PropTypes from 'prop-types';
 
-export const Profile = ({ username, tag, location, avatar, stats }) => {
+export const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+}) => {
   return (
     <div className={css.profile}>
       <div className={css.description}>
@@ -13,17 +20,29 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
       <ul className={css.stats}>
         <li className={css.statBox}>
           <span className={css.label}>Followers</span>
-          <span className={css.quantity}>{stats.followers}</span>
+          <span className={css.quantity}>{followers}</span>
         </li>
         <li className={css.statBoxMiddle}>
           <span className={css.label}>Views</span>
-          <span className={css.quantity}>{stats.views}</span>
+          <span className={css.quantity}>{views}</span>
         </li>
         <li className={css.statBox}>
           <span className={css.label}>Likes</span>
-          <span className={css.quantity}>{stats.likes}</span>
+          <span className={css.quantity}>{likes}</span>
         </li>
       </ul>
     </div>
   );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.shape({
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
+  }),
 };
